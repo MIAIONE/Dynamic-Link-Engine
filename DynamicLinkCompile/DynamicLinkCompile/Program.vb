629300a22e91850440cd4600614ebcc8b352a607
systemroot <miaione@yeah.net>
2021-01-25 16:16:51 +0800

    new

--- DynamicLinkCompile/DynamicLinkCompile/Program.vb
+++ DynamicLinkCompile/DynamicLinkCompile/Program.vb
@@ -1,6 +1,6 @@
 Public Class Program
     Public Shared Sub Main()
-        Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException)
+        ' Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException)
         Dim P As New LinkService
         P.Compile(File.ReadAllText("D:\DLC.LSS"))
     End Sub
